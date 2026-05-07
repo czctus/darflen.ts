b6fad65a31ffbbae2234081090d104868a90d959
czctus <czc@perox.dev>
2026-05-07 13:26:29 -0400

    fix(debug): fixed eslint and typescript error

--- src/utils/debug.ts
+++ src/utils/debug.ts
@@ -3,10 +3,10 @@ import { debugNamespace } from "../constants.js";
 const isBrowser = typeof window !== "undefined" || (typeof process === "undefined" || !process.version);
 
 const debugLit = "debug";
-let debug = isBrowser ? null : await import(debugLit).then((m) => m.default).catch(() => null); // we do this because i don't want webworkers/browsers to bundle it..
+const debug = isBrowser ? null : await import(debugLit).then((m) => m.default).catch(() => null); // we do this because i don't want webworkers/browsers to bundle it..
 
 const create = (namespace: string) => {
-    if (isBrowser || !debug) return (..._args: unknown[]) => { };
+    if (isBrowser || !debug) return () => { };
 
     return (...args: unknown[]) => {
         const debugFn = debug(`${debugNamespace}:${namespace}`);
